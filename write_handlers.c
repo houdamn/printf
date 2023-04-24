da2b21b51433cea6c8d05e8ac29f17916f905200
itsabdelmalek <itsabdelmalek@gmail.com>
2023-04-24 16:36:23 -0700

    update

--- write_handlers.c
+++ write_handlers.c
@@ -2,13 +2,13 @@
 
 /************************* WRITE HANDLE *************************/
 /**
- * handle_write_char - Prints a string
+ * handle_write_char - Prints a string.
  * @c: char types.
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags.
  * @width: get width.
  * @precision: precision specifier
- * @size: Size specifier
+ * @size: Size specifier.
  *
  * Return: Number of chars printed.
  */
